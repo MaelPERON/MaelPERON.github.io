b9ab6ea1be4d7382b8a17e9e3be28986729cc1bf
MaelPERON <49273112+MaelPERON@users.noreply.github.com>
2021-12-26 01:40:27 +0100

    Updated website
    - added menu section
    - added about me section

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 particles = []
 
-function randomNumber(min, max, r=2){
+function randomNumber(min, max, r = 2) {
     return (Math.floor(Math.random() * (max - min)) + min).toFixed(r)
 }
 
 window.onload = () => {
+
     [...Array(10).keys()].forEach(x => {
-        obj = document.createElement("img")
-        obj.setAttribute("src", "./assets/svg/hexagon.svg")
-        obj.setAttribute("style", `position: absolute; top: ${randomNumber(0,100)}%; left: ${randomNumber(0,100)}%;`)
-        particles.push(obj)
-        document.querySelector("#particles").appendChild(obj)
-    })
+
+
+            /* obj = document.createElement("img")
+            obj.setAttribute("src", "./assets/svg/hexagon.svg");
+            [x, y, r] = [
+                [0, 100],
+                [0, 100],
+                [2, 15]
+            ].map(a => randomNumber(a[0], a[1]))
+            obj.setAttribute("style", `position: absolute; top: ${x}%; left: ${y}%; height: ${r}%;`)
+            particles.push([obj, x, y])
+            document.querySelector("#particles").appendChild(obj) */
+        })
+        // requestAnimationFrame(animate)
 }
-
-
-particlesJS.load('particles-js', '../assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
