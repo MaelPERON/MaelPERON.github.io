45bc1a1f14eda483140528ca2d74147a06645592
MaelPERON <maelperonmessagerie2015@gmail.com>
2022-01-08 10:12:06 +0100

    Added navbar

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ function createElementFromHTML(htmlString) {
     // Change this to div.childNodes to support multiple top-level nodes
     return div.firstChild;
 }
+
+document.querySelector("#nav-button").addEventListener("click", e => {
+    toggleNavbar(e.target);
+    e.preventDefault();
+})
+
+function toggleNavbar(button) {
+    button.classList.toggle("active")
+    navbar = document.querySelector("nav.navbar")
+    bars = document.querySelector("#nav-button svg.fa-bars")
+    times = document.querySelector("#nav-button svg.fa-times")
+    if (button.classList.contains("active")) {
+        navbar.style.display = "block"
+        bars.style.display = "none"
+        times.style.display = "block"
+    } else {
+        navbar.style.display = "none"
+        bars.style.display = "block"
+        times.style.display = "none"
+    }
+
+}
