83dbcf5cf2c18131310dc7f4e10d90fc4993a66b
MaelPERON <maelperonmessagerie2015@gmail.com>
2022-04-07 16:35:06 +0200

    Added show more button

--- js/work-page.js
+++ js/work-page.js
@@ -1,1 +1,13 @@
-lazyLoading()
+function toggleMore(button,cards){
+    button.classList.toggle("active")
+    cards.forEach(card => card.style.display = button.classList.contains("active") ? "none" : "")
+    button.innerHTML = button.classList.contains("active") ? `Afficher Plus (${cards.length})` : "Afficher Moins"
+}
+
+document.querySelectorAll("a.more").forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMore(button, Array.from(button.parentElement.querySelectorAll(".grid > .card")).filter((card,i) => i+1 > (button.getAttribute("data-min") ? parseInt(button.getAttribute("data-min")) : 3)))
+    })
+    button.click()
+})
